f6039919aabc71c5cbdf579b05f7aa15c2e25193
NAJMULHASAN001 <najmulhasan2001@gmail.com>
2026-06-05 20:03:49 +0600

    tried to add task status

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,12 +1,13 @@
 import { use, useState } from "react";
 import Main from "./Main";
+import { toast, ToastContainer } from 'react-toastify';
 
 const Cards = ({ dataPromise }) => {
     const initialData = use(dataPromise)
     // const [data, setData] = useState(initialData)
     const [inProgressCount, setInProgressCount] = useState(0);
     const handleClick = () => {
-        console.log('card clicked')
+        toast('In progress')
         setInProgressCount(prev => prev + 1);
     }
     return (
@@ -17,6 +18,8 @@ const Cards = ({ dataPromise }) => {
                 <Main inProgressCount={inProgressCount}
                     setInProgressCount={setInProgressCount}></Main>
                 <section className="md:flex container mx-auto gap-3 mt-20 ">
+
+                    
                     <div className="md:w-3/4 border-1 ">
                         <h1 className="text-3xl font-bold">Customer Tickets</h1>
                         <div className="grid md:grid-cols-2 md:gap-7 mt-3">
@@ -25,7 +28,7 @@ const Cards = ({ dataPromise }) => {
                                     console.log(data)
 
                                     return (
-                                        <div onClick={handleClick} className="card border-1 bg-base-100 card-md shadow-xl ">
+                                        <div onClick={handleClick} className="card border-1 bg-base-100 card-md shadow-xl cursor">
                                             <div className="card-body">
                                                 <div className="md:flex md:justify-between">
                                                     <h2 className="card-title font-bold text-2xl">{data.title}</h2>
@@ -47,12 +50,19 @@ const Cards = ({ dataPromise }) => {
                                 })}
                         </div >
                     </div>
+
                     <div className="border-1 md:w-1/4">
                         <h1 className="text-3xl font-bold">Task Status</h1>
                         <p>Select a ticket to add to Task Status</p>
+                        <div onClick={handleClick} className=" shadow-sm bg-amber-200 border-1 flex flex-col justify-center ">
+                            <h1 className="text-xl font-bold">Login issue</h1>
+                            <button className="bg-green-400 text-white"> Complete</button>
+
+                        </div>
                     </div>
                 </section>
             </div>
+            <ToastContainer></ToastContainer>
         </>
     );
 };
