797daa0b72f4f2341b11af9f867ad61d4b9e0f94
NAJMULHASAN001 <najmulhasan2001@gmail.com>
2026-06-05 10:44:04 +0600

    added res in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,32 +1,32 @@
 const Footer = () => {
     return (
        <>
-     <section className="bg-black text-white hidden">
-         <div className="grid grid-cols-5 gap-30 p-15 mt-10">
+     <section className="bg-black text-white ">
+         <div className="grid md:grid-cols-5 gap-10 md:gap-30 p-15 md:mt-10">
           <div>
-            <h1>CS — Ticket System</h1>
+            <h1 className="text-2xl font-bold">CS — Ticket System</h1>
             <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
         </div>
         <div>
-            <h1>Company</h1>
+            <h1 className="text-2xl font-bold">Company</h1>
             <p>About Us</p>
             <p>Our Mission</p>
             <p>Contact Saled</p>
         </div>
         <div>
-            <h1>Services</h1>
+            <h1 className="text-2xl font-bold">Services</h1>
             <p>Products & Services</p>
             <p>Customer Stories</p>
             <p>Download Apps</p>
         </div>
         <div>
-            <h1>Information</h1>
+            <h1 className="text-2xl font-bold">Information</h1>
             <p>Privacy Policy</p>
             <p>Terms & Conditions</p>
             <p>Join Us</p>
         </div>
         <div>
-            <h1>Social Links</h1>
+            <h1 className="text-2xl font-bold">Social Links</h1>
             <p>@CS — Ticket System</p>
             <p>@CS — Ticket System</p>
             <p>@CS — Ticket System</p>
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -8,13 +8,13 @@ const Main = ({inProgressCount}) => {
     //  const inProgress = data.filter((element) => element.status == "In-Progress, Open");
 //   const resolved = data.filter((element) => element.status == "Open");
     return (
-       <div className='hidden'>
-        <div className="container border-1 mx-auto flex justify-between mt-25" >
-            <div className=" bg-gradient-to-r from-[#632EE3]  to-[#9F62F2] h-[300px] w-[750px] flex flex-col justify-center items-center rounded-2xl">
+       <div className=''>
+        <div className="container border-1 mx-auto md:flex md:justify-between mt-25" >
+            <div className=" bg-gradient-to-r from-[#632EE3]  to-[#9F62F2] h-[300px] md:w-[750px] flex flex-col justify-center items-center rounded-2xl mb-5 md:mb-0">
                 <p className=" text-5xl font-bold text-white mb-3">In progress</p>
                 <p className="text-8xl font-bold text-white">{inProgressCount}</p>
             </div>
-            <div className="bg-gradient-to-r from-[#54CF68]  to-[#00827A] h-[300px] w-[750px] flex flex-col justify-center items-center rounded-2xl">
+            <div className="bg-gradient-to-r from-[#54CF68]  to-[#00827A] h-[300px] md:w-[750px] flex flex-col justify-center items-center rounded-2xl">
                 <p className="text-5xl font-bold text-white mb-3">Resolved</p>
                 <p className="text-8xl font-bold text-white">0</p>
             </div>
